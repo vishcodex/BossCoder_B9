77d22627abc8cd436aa73478462b5fdfbc7032c4
Putta, Vishnu <vishnu.putta@hotmail.com>
2022-07-23 20:39:42 -0500

    Adding solution for richest customer

--- richest-customer-wealth.py
+++ richest-customer-wealth.py
@@ -2,3 +2,33 @@
 
 # A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
 
+from typing import List
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        # n = len(accounts)
+        # m = len(accounts[0])
+
+        val = 0
+
+        for i in accounts:
+           if(sum(i) >= val):
+            val = sum(i)
+            return val
+
+
+        # for i in range(n):
+        #     tot = 0
+        #     for j in range(m):
+        #         # print(i,j)
+        #         tot += accounts[i][j]
+        #         total_sum.append(tot)
+
+
+        # return max(total_sum)
+
+if __name__ == '__main__':
+    val = [[1,2,3],[3,2,1]]
+    ob = Solution()
+    res = ob.maximumWealth(val)
+    print(res)
